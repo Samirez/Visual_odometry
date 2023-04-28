19818c9a1c554b2b0a88ba9a6261306c7a14bb0f
Zain Ahmed <zaahm18@student.sdu.dk>
2023-04-28 13:48:52 +0200

    added images

---
+++ triangulate.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+    
+    
+def main():
+    filelocation = "./output/images/"
+    frame1 = cv2.imread(filelocation + 'DJI_0199_1200.jpg')
+    frame2 = cv2.imread(filelocation + 'DJI_0199_1300.jpg')
+    
+    cv2.imshow("frame1",frame1)
+    cv2.imshow("frame2",frame2)
+
+
+if __name__ == "__main__":
+    main()
