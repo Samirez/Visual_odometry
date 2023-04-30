7d19ee0d1b81b963233ae6bd4a18d8fbe3577a5e
ahnai17 <ahnai17@student.sdu.dk>
2023-04-30 15:12:22 +0200

    Update feature_detection.py

--- src/feature_detection.py
+++ src/feature_detection.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-frame1 = cv2.imread("output/images/DJI_0199_1200.jpg")
+frame1 = cv2.imread("../output/images/DJI_0199_1200.jpg")
 sift1 = cv2.SIFT_create()
 keypoints1, descriptors1 = sift1.detectAndCompute(frame1, None)
 img1 = cv2.drawKeypoints(frame1, keypoints1, frame1, cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-frame2 = cv2.imread("output/images/DJI_0199_1250.jpg")
+frame2 = cv2.imread("../output/images/DJI_0199_1250.jpg")
 sift2 = cv2.SIFT_create()
 keypoints2, descriptors2 = sift2.detectAndCompute(frame2, None)
 img2 = cv2.drawKeypoints(frame2, keypoints2, frame2, cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
